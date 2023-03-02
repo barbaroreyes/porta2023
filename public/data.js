a4d9099f6b1e711ef7d0ba4a1b81dc55ef85908e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-02 14:22:27 -0600

    data

---
+++ public/data.js
@@ -0,0 +1,59 @@
+export  const data = [
+    { 
+         "id": Math.random.toString(8),
+        "name":"name1",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+
+},
+     {  "id": Math.random.toString(8),
+     "name":"name2",
+     "live":"live",
+     "Gib" : "gib",
+     "picture":"picture"
+
+     }, {
+        "id": Math.random.toString(8),
+        "name":"nam3",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+
+     }, {
+        "id": Math.random.toString(8),
+        "name":"name3",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+     }, {
+        "id": Math.random.toString(8),
+        "name":"name5",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+     }, {
+        "id": Math.random.toString(8),
+        "name":"name6",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+     }, {
+        "id": Math.random.toString(8),
+        "name":"name7",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+     }, {
+        "id": Math.random.toString(8),
+        "name":"name8",
+        "live":"live",
+        "Gib" : "gib",
+        "picture":"picture"
+   
+     }]
