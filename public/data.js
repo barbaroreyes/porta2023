34818c06d37099729c348e0d19518987012e3fa7
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-02 14:30:59 -0600

    update

--- public/data.js
+++ public/data.js
@@ -1,10 +1,10 @@
 export  const data = [
     { 
          "id": Math.random.toString(8),
-        "name":"name1",
-        "live":"live",
-        "Gib" : "gib",
-        "picture":"picture"
+        "name":"Beers",
+        "live":"https://barbaro-store-beers.netlify.app/",
+        "Gib" : "https://github.com/barbaroreyes/app-store-beers",
+        "picture":"https://github.com/barbaroreyes/app-store-beers/blob/main/src/components/images/project.jpeg?raw=true"
 
 },
      {  "id": Math.random.toString(8),
