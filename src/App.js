d08cb2b0f462a1ea6855d1e4bd89a4535932b9bc
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-03 11:45:47 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import {Routes ,Route} from 'react-router-dom'
 import './App.css';
 import Projects from './compo/Projects';
+import Header from './compo/Header';
+import Home from './pages/Home';
+import Footer from './compo/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header/>
     <Routes>
-      <Route path="/" element ={<Projects/>}/>
+    <Route path="/" element ={<Home/>}/>
+      <Route path="/projects" element ={<Projects/>}/>
     </Routes>
-     
-    </div>
+     <Footer/>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div>picture</div>
+      <div>main</div>
+    </div>
+  );
+}
+
+export default Home;
