c6e3e843fcd12bc52f8ec059c6272f66eedb5410
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-09-26 16:13:43 -0500

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Header/>
     <Routes>
     <Route path="/" element ={<Home/>}/>
-      <Route path="/projects" element ={<Projects/>}/>
+    <Route path="/projects" element ={<Projects/>}/>
     </Routes>
      <Footer/>
     </div>
